8938f1495abbdc090255171ea58f963b1c8813aa
Floyd Pink <imbleedingme@googlemail.com>
2014-09-12 02:39:02 -0400

    renamed app to mean-app

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,117 +9,144 @@ module.exports = function (grunt) {
 
   // configurable paths
   var config = {
-    app: 'app',
-    dist: 'dist',
-    tmp: 'tmp',
-    resources: 'resources'
+    app       : 'mean-app',
+    dist      : 'dist',
+    tmp       : 'tmp',
+    resources : 'resources'
   };
 
   grunt.initConfig({
-    config: config,
-    clean: {
-      dist: {
-        files: [{
-          dot: true,
-          src: [
-            '<%= config.dist %>/*',
-            '<%= config.tmp %>/*'
-          ]
-        }]
+    config   : config,
+    clean    : {
+      dist : {
+        files : [
+          {
+            dot : true,
+            src : [
+              '<%= config.dist %>/*',
+              '<%= config.tmp %>/*'
+            ]
+          }
+        ]
       }
     },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
+    jshint   : {
+      options : {
+        jshintrc : '.jshintrc'
       },
-      files: '<%= config.app %>/js/*.js'
+      files   : '<%= config.app %>/js/*.js'
     },
-    copy: {
-      appLinux: {
-        files: [{
-          expand: true,
-          cwd: '<%= config.app %>',
-          dest: '<%= config.dist %>/app.nw',
-          src: '**'
-        }]
+    copy     : {
+      appLinux     : {
+        files : [
+          {
+            expand : true,
+            cwd    : '<%= config.app %>',
+            dest   : '<%= config.dist %>/app.nw',
+            src    : '**'
+          }
+        ]
       },
-      appMacos: {
-        files: [{
-          expand: true,
-          cwd: '<%= config.app %>',
-          dest: '<%= config.dist %>/node-webkit.app/Contents/Resources/app.nw',
-          src: '**'
-        }, {
-          expand: true,
-          cwd: '<%= config.resources %>/mac/',
-          dest: '<%= config.dist %>/node-webkit.app/Contents/',
-          filter: 'isFile',
-          src: '*.plist'
-        }, {
-          expand: true,
-          cwd: '<%= config.resources %>/mac/',
-          dest: '<%= config.dist %>/node-webkit.app/Contents/Resources/',
-          filter: 'isFile',
-          src: '*.icns'
-        }, {
-          expand: true,
-          cwd: '/../node_modules/',
-          dest: '/node-webkit.app/Contents/Resources/app.nw/node_modules/',
-          src: '**'
-        }]
+      appMacos     : {
+        files : [
+          {
+            expand : true,
+            cwd    : '<%= config.app %>',
+            dest   : '<%= config.dist %>/node-webkit.app/Contents/Resources/app.nw',
+            src    : '**'
+          },
+          {
+            expand : true,
+            cwd    : '<%= config.resources %>/mac/',
+            dest   : '<%= config.dist %>/node-webkit.app/Contents/',
+            filter : 'isFile',
+            src    : '*.plist'
+          },
+          {
+            expand : true,
+            cwd    : '<%= config.resources %>/mac/',
+            dest   : '<%= config.dist %>/node-webkit.app/Contents/Resources/',
+            filter : 'isFile',
+            src    : '*.icns'
+          },
+          {
+            expand : true,
+            cwd    : '/../node_modules/',
+            dest   : '/node-webkit.app/Contents/Resources/app.nw/node_modules/',
+            src    : '**'
+          }
+        ]
       },
-      webkit: {
-        files: [{
-          expand: true,
-          cwd: '<%=config.resources %>/node-webkit/MacOS',
-          dest: '<%= config.dist %>/',
-          src: '**'
-        }]
+      webkit       : {
+        files : [
+          {
+            expand : true,
+            cwd    : '<%=config.resources %>/node-webkit/MacOS',
+            dest   : '<%= config.dist %>/',
+            src    : '**'
+          }
+        ]
       },
-      copyWinToTmp: {
-        files: [{
-          expand: true,
-          cwd: '<%= config.resources %>/node-webkit/Windows/',
-          dest: '<%= config.tmp %>/',
-          src: '**'
-        }]
+      copyWinToTmp : {
+        files : [
+          {
+            expand : true,
+            cwd    : '<%= config.resources %>/node-webkit/Windows/',
+            dest   : '<%= config.tmp %>/',
+            src    : '**'
+          }
+        ]
       }
     },
-    compress: {
-      appToTmp: {
-        options: {
-          archive: '<%= config.tmp %>/app.zip'
+    compress : {
+      appToTmp        : {
+        options : {
+          archive : '<%= config.tmp %>/app.zip'
         },
-        files: [{
-          expand: true,
-          cwd: '<%= config.app %>',
-          src: ['**']
-        }]
+        files   : [
+          {
+            expand : true,
+            cwd    : '<%= config.app %>',
+            src    : ['**']
+          }
+        ]
       },
-      finalWindowsApp: {
-        options: {
-          archive: '<%= config.dist %>/swaraServer.zip'
+      finalWindowsApp : {
+        options : {
+          archive : '<%= config.dist %>/swaraServer.zip'
         },
-        files: [{
-          expand: true,
-          cwd: '<%= config.tmp %>',
-          src: ['**']
-        }]
+        files   : [
+          {
+            expand : true,
+            cwd    : '<%= config.tmp %>',
+            src    : ['**']
+          }
+        ]
       }
     },
-    rename: {
-      app: {
-        files: [{
-          src: '<%= config.dist %>/node-webkit.app',
-          dest: '<%= config.dist %>/swaraServer.app'
-        }]
+    rename   : {
+      app      : {
+        files : [
+          {
+            src  : '<%= config.dist %>/node-webkit.app',
+            dest : '<%= config.dist %>/swaraServer.app'
+          }
+        ]
       },
-      zipToApp: {
-        files: [{
-          src: '<%= config.tmp %>/app.zip',
-          dest: '<%= config.tmp %>/app.nw'
-        }]
+      zipToApp : {
+        files : [
+          {
+            src  : '<%= config.tmp %>/app.zip',
+            dest : '<%= config.tmp %>/app.nw'
+          }
+        ]
       }
+    },
+    hub      : {
+      all : {
+        src   : ['<%= config.app %>/Gruntfile.js'],
+        tasks : ['jshint', 'nodeunit'],
+      },
     }
   });
 
@@ -135,7 +162,7 @@ module.exports = function (grunt) {
     var done = this.async();
     var childProcess = require('child_process');
     var exec = childProcess.exec;
-    exec('mkdir -p ./dist; cp resources/node-webkit/'+ version +'/nw.pak dist/ && cp resources/node-webkit/'+ version +'/nw dist/node-webkit', function (error, stdout, stderr) {
+    exec('mkdir -p ./dist; cp resources/node-webkit/' + version + '/nw.pak dist/ && cp resources/node-webkit/' + version + '/nw dist/node-webkit', function (error, stdout, stderr) {
       var result = true;
       if (stdout) {
         grunt.log.write(stdout);
@@ -196,23 +223,23 @@ module.exports = function (grunt) {
     var mainPackageJSON = grunt.file.readJSON('package.json');
     var appPackageJSON = grunt.file.readJSON(appPath + '/package.json');
     var infoPlistTmp = grunt.file.read(resourcesPath + '/mac/Info.plist.tmp', {
-      encoding: 'UTF8'
+      encoding : 'UTF8'
     });
     var infoPlist = grunt.template.process(infoPlistTmp, {
-      data: {
-        version: version
+      data : {
+        version : version
       }
     });
     mainPackageJSON.version = version;
     appPackageJSON.version = version;
     grunt.file.write('package.json', JSON.stringify(mainPackageJSON, null, 2), {
-      encoding: 'UTF8'
+      encoding : 'UTF8'
     });
     grunt.file.write(appPath + '/package.json', JSON.stringify(appPackageJSON, null, 2), {
-      encoding: 'UTF8'
+      encoding : 'UTF8'
     });
     grunt.file.write(resourcesPath + '/mac/Info.plist', infoPlist, {
-      encoding: 'UTF8'
+      encoding : 'UTF8'
     });
   });
 
