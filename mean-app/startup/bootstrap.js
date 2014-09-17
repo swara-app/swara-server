398868716f7dad348df0c0b6e59a8892ff454e04
Floyd Pink <imbleedingme@googlemail.com>
2014-09-17 08:31:53 -0400

    node-main is now used for daemon; attempted tree-kill unsuccessfully

---
+++ mean-app/startup/bootstrap.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var debug = require('debug')('swara:bootstrap');
+
+var run = function () {
+  debug('Entered bootstrap function');
+  var gui = require('nw.gui'), // Load native UI library
+      win = require('nw.gui').Window.get(), // Get the current window
+      platform = require('os').platform(),
+      kill = require('tree-kill');
+
+  // attach mac menu
+  if (platform === 'darwin') {
+    debug('Attaching the Mac menu');
+    var nativeMenuBar = new gui.Menu({ type : 'menubar' });
+    nativeMenuBar.createMacBuiltin('Swara Server', {
+      hideEdit   : true,
+      hideWindow : true
+    });
+    win.menu = nativeMenuBar;
+  }
+
+  // open Dev Tools
+  // win.showDevTools();
+
+  // set up the shutdown of the server
+  // TODO: Doesn't work now
+  debug('Setting up the eventhandler for server teardown');
+  win.on('close', function () {
+    debug('Entered the close event');
+    this.hide();
+    var pid = process.pid;
+    debug('About to issue a tree-kill on the process id: ' + pid);
+    kill(pid);
+    this.close(true);
+  });
+
+
+  // navigate to the meanjs app
+  // start TODO
+  // put this in an infinite loop and keep checking the entry
+  // 'MEAN.JS application started on port 3000' to appear on stdout.log
+  // and then navigate to localhost
+  // end TODO
+  setTimeout(function () {
+    debug('Navigating to the meanjs app');
+    window.location.assign('http://localhost:3000/');
+  }, 1000);
+};
---
+++ mean-app/startup/daemon.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var debug = require('debug')('swara:daemon');
+
+debug('Declaring daemon:start function');
+var daemon = {
+  start : function () {
+    debug('Entered daemon:start function');
+    var spawn = require('child_process').spawn,
+        fs = require('fs'),
+        util = require('util'),
+        mkdirp = require('mkdirp');
+
+    // start the server process
+    debug('Starting the server');
+    var outputDirectory = 'logs';
+    mkdirp(outputDirectory, function (err) {
+      if (err) {
+        console.error(err);
+      }
+      var stdoutfile = fs.openSync(outputDirectory + '/stdout.log', 'w+');
+      var stderrfile = fs.openSync(outputDirectory + '/stderr.log', 'w+');
+
+      var startMarker = util.format('Session started at %s\n--------------\n', new Date().toUTCString());
+      fs.writeSync(stdoutfile, startMarker);
+      fs.writeSync(stderrfile, startMarker);
+
+      var server = spawn('node', ['startup/www.js'], {
+        env   : process.env,
+        stdio : ['ignore', stdoutfile, stderrfile]
+      });
+
+    });
+  }
+};
+
+daemon.start();
+
+module.exports = daemon;
