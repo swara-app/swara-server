5e55405db262e9e91e63146ace384cf0a648a806
Floyd Pink <imbleedingme@googlemail.com>
2014-09-22 01:38:06 -0400

    WIP directive for directory picker

--- mean-app/public/modules/folders/directives/directorypicker.client.directive.js
+++ mean-app/public/modules/folders/directives/directorypicker.client.directive.js
@@ -1,16 +1,31 @@
 'use strict';
 
-angular.module('folders').directive('directoryPicker', [
-	function() {
-		return {
-			template: '<div></div>',
-			restrict: 'E',
-			link: function postLink(scope, element, attrs) {
-				// Directorypicker directive logic
-				// ...
-        console.log('>>> Inside the link function in the directoryPicker directive');
-				element.text('this is the directorypicker directive');
-			}
-		};
-	}
+angular.module('folders').directive('directoryPicker', ['$compile',
+  function ($compile) {
+    return {
+      restrict    : 'E',
+      templateUrl : 'modules/folders/templates/directorypicker.client.template.html',
+      link        : function (scope, element, attrs, ngModel) {
+        var span = element.find('span'),
+            button = element.find('button');
+
+        var setSpanValue = function () {
+          span.text(scope.path || attrs.placeholder);
+        };
+
+        setSpanValue();
+
+        button.on('click', function () {
+          var input = document.createElement('input');
+          input.setAttribute('type', 'file');
+          input.setAttribute('nwdirectory', 'true');
+          input.onchange = function (event) {
+            scope.path = this.value;
+            setSpanValue();
+          };
+          input.click();
+        });
+      }
+    };
+  }
 ]);
