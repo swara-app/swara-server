43b3921c554032b1d80c599d382217eff66c3436
Floyd Pink <imbleedingme@googlemail.com>
2015-01-09 10:20:47 -0500

    worker for scanner - WIP

---
+++ mean-app/app/workers/scanner/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*
+ Scans the folders when they are being added to the library for the first time,
+ as well as when the folders are explicitly being rescanned from the UI
+ */
+
+var debug = require('debug')('swara:scanner'),
+  fs = require('fs');
+
+var scanner = {
+  scanFolder : function (folderPath) {
+    
+  }
+};
+
+module.exports = scanner;
