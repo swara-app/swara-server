58eec0b8f913913d3e9497d167b5882887baf0b0
Floyd Pink <imbleedingme@googlemail.com>
2015-01-22 20:00:54 -0500

    tweaked the server shutdown process

--- mean-app/startup/injectJs.js
+++ mean-app/startup/injectJs.js
@@ -2,28 +2,24 @@
 
 var debug = require('debug')('swara:injectJs'),
   win = require('nw.gui').Window.get(),
-  daemon = require('./daemon.js'),
-  kill = require('tree-kill');
-
-function killProcessTree(pid) {
-  debug('About to issue a tree-kill on the process id: ' + pid);
-  kill(pid, 'SIGTERM');
-}
+  daemon = require('./daemon.js');
 
 // setting up the shutdown of the server as window.close eventhandler
-// TODO: This works most of the times, but is still hit or miss!
-// TODO: Need to work on a proper server shutdown
 debug('Setting up the eventhandler for server teardown');
 win.on('close', function () {
   debug('Entered the close event');
   // First hide the window
   this.hide();
 
-  // kill the server child process
-  killProcessTree(daemon.getServerPid());
+  // kill express server and its child processes
+  debug('Killing the server process..');
+  process.kill(daemon.getServerPid(), 'SIGKILL');
 
-  // kill the main process
-  killProcessTree(process.pid);
+  // exit the main nw process
+  setTimeout(function () {
+    debug('Exiting main process..');
+    process.exit(0);
+  }, 100);
 
   debug('Closing the window...');
   this.close(true);
