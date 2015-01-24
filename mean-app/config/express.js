3cc8d2ae97e898a2a4429278fd5bb158175e6962
Floyd Pink <imbleedingme@googlemail.com>
2015-01-24 15:00:28 -0500

    barely got socket.io to work with article create - can't shutdown properly and there is duplicates abound

--- mean-app/config/express.js
+++ mean-app/config/express.js
@@ -6,6 +6,8 @@
 var fs = require('fs'),
   https = require('https'),
   express = require('express'),
+  http = require('http'),
+  socketio = require('socket.io'),
   morgan = require('morgan'),
   bodyParser = require('body-parser'),
   session = require('express-session'),
@@ -158,6 +160,12 @@ module.exports = function (db) {
     // Return HTTPS server instance
     return httpsServer;
   }
+
+  // Attach Socket.io
+  var server = http.createServer(app);
+  var io = socketio.listen(server);
+  app.set('socketio', io);
+  app.set('server', server);
 
   // Return Express server instance
   return app;
--- mean-app/public/config.js
+++ mean-app/public/config.js
@@ -6,7 +6,7 @@ var ApplicationConfiguration = (function () {
   var applicationModuleName = 'swara-server';
   var applicationModuleVendorDependencies = [
     'ngResource', 'ngCookies', 'ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'ui.utils',
-    'cgPrompt', 'angularUtils.directives.dirPagination', 'ncy-angular-breadcrumb'
+    'cgPrompt', 'angularUtils.directives.dirPagination', 'ncy-angular-breadcrumb', 'btford.socket-io'
   ];
 
   // Add a new vertical module
