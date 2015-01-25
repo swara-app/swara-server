5ea5f9d11052a559a80b8e87fb8939d788595471
Floyd Pink <imbleedingme@googlemail.com>
2015-01-25 03:42:23 -0500

    timestamp also into log header

--- mean-app/public/modules/server/controllers/server.client.controller.js
+++ mean-app/public/modules/server/controllers/server.client.controller.js
@@ -14,7 +14,15 @@ angular.module('server').controller('ServerController', ['$scope', '$sce', '$sta
     };
 
     $scope.getLogHeader = function (logInfoLine) {
-      return logInfoLine.slice(logInfoLine.indexOf('`') + 1, logInfoLine.lastIndexOf('`'));
+      var beginAction = logInfoLine.indexOf('`');
+      var endAction = logInfoLine.lastIndexOf('`');
+      var action = logInfoLine.slice(beginAction + 1, endAction);
+
+      var beginTimeStamp = logInfoLine.indexOf('(', endAction);
+      var endTimestamp = logInfoLine.lastIndexOf(')') + 1;
+      var timestamp = logInfoLine.slice(beginTimeStamp, endTimestamp);
+
+      return (action + ' ' + timestamp).replace(/`/g, '');
     };
 
     $scope.init = function () {
